8cb08ea6c4ac31e0f915355e7d3741f98eacbd4a
Shaun <shaun.mccrum@dcmail.ca>
2019-03-22 12:36:20 -0400

    Repurposed Classes added for Rank and Suit Classes.

---
+++ DurakClassLibrary/Rank.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Suit.cs - Enumerator for card ranks.
+ * 
+ * Author: Shaun McCrum
+ * Since: 22 Mar 2019
+ * See: Watson, K. (2013). Beginning Visual C♯® 2012 programming. Indianapolis, IN: John Wiley & Sons.
+ * 
+ */
+
+
+namespace DurakClassLibrary
+{
+    public enum Rank
+    {
+        Ace = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7,
+        Eight = 8,
+        Nine = 9,
+        Ten = 10,
+        Jack = 11,
+        Queen = 12,
+        King = 13
+    }
+}
---
+++ DurakClassLibrary/Suit.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * Suit.cs - Enumerator for card suits.
+ * 
+ * Author: Shaun McCrum
+ * Since: 22 Mar 2019
+ * See: Watson, K. (2013). Beginning Visual C♯® 2012 programming. Indianapolis, IN: John Wiley & Sons.
+ * 
+ */
+
+namespace DurakClassLibrary
+{
+    public enum Suit
+    {
+        Club,
+        Diamond,
+        Heart,
+        Spade
+    }
+}

473664389ddcb6c9b8a3e9921cba3ab4647862f4
Shaun_Mc <shaun.k.mccrum@gmail.com>
2019-03-24 17:20:00 -0400

    Table default colour adjustment.

--- DurakProject/DurakForm.Designer.cs
+++ DurakProject/DurakForm.Designer.cs
@@ -226,7 +226,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(47)))), ((int)(((byte)(33)))));
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(1091, 606);
             this.Controls.Add(this.lblLosses);

dbeb25e0ef64c0986c1f51a2211ae2caed8c6556
Shaun_Mc <shaun.k.mccrum@gmail.com>
2019-03-24 21:03:08 -0400

    organized code to make it easier to follow

--- DurakClassLibrary/Player.cs
+++ DurakClassLibrary/Player.cs
@@ -3,6 +3,7 @@ namespace DurakClassLibrary
 {
     class Player
     {
+        #region CONSTRUCTORS AND FIELD PROPERTIES
         public string Name { get; private set; }
 
         public Cards Hand { get; private set; }
@@ -14,17 +15,25 @@ namespace DurakClassLibrary
             Name = name;
             Cards PlayHand = new Cards();
         }
+        #endregion
 
+        #region METHODS
+        /// <summary>
+        /// isEmpty boolena method to determine if the hand is empty
+        /// </summary>
+        /// <returns>is empty</returns>
         public bool HandEmpty()
         {
             bool isEmpty = false;
-            
-            if(Hand.Count == 0)
+
+            if (Hand.Count == 0)
             {
                 isEmpty = true;
             }
 
             return isEmpty;
         }
+        #endregion
+
     }
 }

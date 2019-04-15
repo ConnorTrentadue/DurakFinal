0cfb14dcfc3ff2bdd1d263a804053f247b935f99
Shaun_Mc <shaun.k.mccrum@gmail.com>
2019-04-14 23:03:00 -0400

    negligible

--- DurakProject/DurakForm.cs
+++ DurakProject/DurakForm.cs
@@ -807,7 +807,6 @@ namespace DurakProject
                                     i += 100;
 
                                 }
-
                             }
                         }
                         // flag that a play was made and realign cards

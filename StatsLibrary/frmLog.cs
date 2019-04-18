ab9415d0f378a33d7c92f039197e5ef15c096893
Connor Trentadue <connor.trentadue@dcmail.ca>
2019-04-18 15:37:58 -0400

    changed log form to hide instead of close on user close.

--- StatsLibrary/frmLog.cs
+++ StatsLibrary/frmLog.cs
@@ -26,11 +26,16 @@ namespace StatsLibrary
         private void frmLog_FormClosing(object sender, FormClosingEventArgs e)
         {
             Stats.WriteLog(txtLog.Text);
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                Hide();
+            }
         }
 
         public void WriteToLog(string log)
         {
-            if (txtLog != null)
+            if (!txtLog.IsDisposed)
                 txtLog.AppendText(log+"\n");
         }
     }
